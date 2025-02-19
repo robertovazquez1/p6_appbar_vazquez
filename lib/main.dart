4b997ff916e02df5e30d84463a476343d4e9e40c
User_JXRR <a.22308051281326@cbtis128.edu.mx>
2025-02-19 20:33:26 +0000

    enviar practica 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Vazquez Flutter',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,38 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Vazquez AppBar'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.remove_red_eye_outlined),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.remove_red_eye_outlined),
+            onPressed: () {},
+          )
+        ],
+        leading: const Icon(Icons.icecream),
+        backgroundColor: Colors.blueAccent,
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
+      backgroundColor: Colors.white,
+      body: const Center(
         child: Text(
-          'Hello, World!',
+          'Hola Roberto Vazquez con AppBar',
         ),
       ),
     );
